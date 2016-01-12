7a2ebce802100ee2b2a1d766fb5274d29a21c2f7
Jean-François YUEN <jfyuen@gmail.com>
2016-01-13 00:51:21 +0100

    Smaller tests

--- packaging_test.go
+++ packaging_test.go
@@ -144,62 +144,51 @@ func TestPackage(t *testing.T) {
 }
 
 func TestProductPackage(t *testing.T) {
-	product, err := Scrap("7613034383808")
-	if err != nil {
-		t.Error(err)
+	product := Product{EAN: "7613034383808", Name: "Four à Pierre Royale", URL: "http://fr.openfoodfacts.org/api/v0/produit/7613034383808.json", ImageURL: "http://static.openfoodfacts.org/images/products/761/303/438/3808/front.8.400.jpg"}
+	pp := NewProductPackage(product)
+	materials := []Material{
+		Material{id: 0, Name: "Cardboard box"},
+		Material{id: 1, Name: "Plastic foil"},
+		Material{id: 4, Name: "Food"}}
+	if pp.Product != product {
+		t.Errorf("Some attributes are invalid for: %v; expected %v", pp, product)
+	}
+
+	if len(pp.materials) != len(materials) {
+		t.Errorf("Packages for %v differ", pp.EAN)
 	} else {
-		pp := NewProductPackage(*product)
-		materials := []Material{
-			Material{id: 0, Name: "Cardboard box"},
-			Material{id: 1, Name: "Plastic foil"},
-			Material{id: 4, Name: "Food"}}
-		if pp.Name != "Four à Pierre Royale" || pp.EAN != "7613034383808" ||
-			pp.URL != "http://fr.openfoodfacts.org/api/v0/produit/7613034383808.json" ||
-			pp.ImageURL != "http://static.openfoodfacts.org/images/products/761/303/438/3808/front.8.400.jpg" {
-			t.Errorf("Some attributes are invalid for: %v", pp)
+		for i, m := range materials {
+			pkgMaterial := pp.materials[i]
+			if m.id != pkgMaterial.id || m.Name != pkgMaterial.Name {
+				t.Errorf("Material differ for EAN %v: %v vs %v", pp.EAN, m, pkgMaterial)
+			}
 		}
 
-		if len(pp.materials) != len(materials) {
-			t.Errorf("Packages for %v differ", pp.EAN)
-		} else {
-			for i, m := range materials {
-				pkgMaterial := pp.materials[i]
-				if m.id != pkgMaterial.id || m.Name != pkgMaterial.Name {
-					t.Errorf("Material differ for EAN %v: %v vs %v", pp.EAN, m, pkgMaterial)
+		binNames := map[string]string{"Cardboard box": "Yellow Bin", "Plastic foil": "Green Bin", "Food": "Green Bin"}
+		i := 0
+		for m, bins := range pp.ThrowAway() {
+			if len(bins) != 1 {
+				t.Errorf("material %v must belongs to 1 bin, found %v", m.Name, len(bins))
+			} else {
+				if bins[0].Name != binNames[m.Name] {
+					t.Errorf("material %v belong to %v, not %v", m.Name, binNames[m.Name], bins[0].Name)
 				}
 			}
-
-			binNames := map[string]string{"Cardboard box": "Yellow Bin", "Plastic foil": "Green Bin", "Food": "Green Bin"}
-			i := 0
-			for m, bins := range pp.ThrowAway() {
-				if len(bins) != 1 {
-					t.Errorf("material %v must belongs to 1 bin, found %v", m.Name, len(bins))
-				} else {
-					if bins[0].Name != binNames[m.Name] {
-						t.Errorf("material %v belong to %v, not %v", m.Name, binNames[m.Name], bins[0].Name)
-					}
-				}
-				i++
-			}
+			i++
 		}
 	}
 }
 
 func TestThrowAwayJson(t *testing.T) {
-	product, err := Scrap("7613034383808")
+	product := Product{EAN: "7613034383808"}
+	pkg := NewProductPackage(product)
+	expected := `{"Cardboard box":[{"Name":"Yellow Bin"}],"Food":[{"Name":"Green Bin"}],"Plastic foil":[{"Name":"Green Bin"}]}`
+	out, err := pkg.ThrowAwayJson()
 	if err != nil {
 		t.Error(err)
 	} else {
-		pkg := NewProductPackage(*product)
-		expected := `{"Cardboard box":[{"Name":"Yellow Bin"}],"Food":[{"Name":"Green Bin"}],"Plastic foil":[{"Name":"Green Bin"}]}`
-		out, err := pkg.ThrowAwayJson()
-		if err != nil {
-			t.Error(err)
-		} else {
-
-			if string(out) != expected {
-				t.Errorf("ThrowAwayJson not as expected: %v %v", string(out), expected)
-			}
+		if string(out) != expected {
+			t.Errorf("ThrowAwayJson not as expected: %v %v", string(out), expected)
 		}
 	}
 }
